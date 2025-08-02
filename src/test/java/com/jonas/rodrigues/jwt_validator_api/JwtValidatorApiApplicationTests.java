44265754aca5131d380c3117ea17e9275bbfda4d
Jonas Henrique <programeicafe@outlook.com>
2025-08-02 13:28:42 -0300

    first commit

---
+++ src/test/java/com/jonas/rodrigues/jwt_validator_api/JwtValidatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonas.rodrigues.jwt_validator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtValidatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
